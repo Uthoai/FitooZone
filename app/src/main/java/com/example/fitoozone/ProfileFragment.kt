a13491236441dd5c16a2b71a2fd0e5150e73391a
uthoai <uthoaimarma597@gmail.com>
2024-05-29 16:15:29 +0600

    TrainingFragment

--- app/src/main/java/com/example/fitoozone/ProfileFragment.kt
+++ app/src/main/java/com/example/fitoozone/ProfileFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class ProfileFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+import com.example.fitoozone.adapter.WorkoutItemAdapter
+import com.example.fitoozone.base.BaseFragment
+import com.example.fitoozone.data.WorkoutItemData
+import com.example.fitoozone.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+
+    override fun setListener() {
+
+
+    }
+
+    override fun allObserver() {
     }
 }
--- app/src/main/java/com/example/fitoozone/TrainingFragment.kt
+++ app/src/main/java/com/example/fitoozone/TrainingFragment.kt
@@ -5,55 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.fitoozone.adapter.WorkoutItemAdapter
+import com.example.fitoozone.base.BaseFragment
+import com.example.fitoozone.data.WorkoutItemData
+import com.example.fitoozone.databinding.FragmentTrainingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TrainingFragment : BaseFragment<FragmentTrainingBinding>(FragmentTrainingBinding::inflate) {
+    private lateinit var workoutItemAdapter: WorkoutItemAdapter
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TrainingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TrainingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    val workoutsItem = listOf(
+        WorkoutItemData(R.drawable.workouts_1,"Rapid Lower Body"),
+        WorkoutItemData(R.drawable.workouts_2,"Bodyweight Stretch"),
+        WorkoutItemData(R.drawable.workouts_3,"Core Control"),
+        WorkoutItemData(R.drawable.workouts_4,"Glutes & Abs"),
+        WorkoutItemData(R.drawable.workouts_1,"Rapid Lower Body"),
+        WorkoutItemData(R.drawable.workouts_2,"Bodyweight Stretch"),
+        WorkoutItemData(R.drawable.workouts_3,"Core Control"),
+        WorkoutItemData(R.drawable.workouts_4,"Glutes & Abs"),
+        WorkoutItemData(R.drawable.workouts_1,"Rapid Lower Body"),
+        WorkoutItemData(R.drawable.workouts_2,"Bodyweight Stretch"),
+        WorkoutItemData(R.drawable.workouts_3,"Core Control"),
+        WorkoutItemData(R.drawable.workouts_4,"Glutes & Abs"),
+    )
+    override fun setListener() {
+        workoutItemAdapter = WorkoutItemAdapter(workoutsItem)
+        binding.workoutItemRecycler.adapter = workoutItemAdapter
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_training, container, false)
+    override fun allObserver() {
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TrainingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TrainingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
